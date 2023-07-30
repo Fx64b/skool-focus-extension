9df3362ec2603d0d4443c5e65c1256839e016f7e
Fx64b <90456762+Fx64b@users.noreply.github.com>
2023-07-30 16:21:45 +0200

    feat: toggle elements across different tabs (wip)

--- background.js
+++ background.js
@@ -1,13 +1,27 @@
 function toggleElement() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs.length > 0 && tabs[0].url.includes("skool.com")) {
-        chrome.tabs.sendMessage(tabs[0].id, { message: "toggle_element" }, function (response) {
-            console.log(response);
-        });
+    chrome.tabs.query({}, function (tabs) {
+      for (const tab of tabs) {
+        if (tab.favIconUrl && tab.url.includes("skool.com")) {
+          chrome.tabs.sendMessage(tab.id, { message: "toggle_element" });
         }
+      }
     });
-}
-
-chrome.action.onClicked.addListener(function (tab) {
+  }
+  
+  chrome.action.onClicked.addListener(function (tab) {
     toggleElement();
-});
+  });
+  
+  // Listen for messages from the content script or other tabs to toggle elements
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.message === "toggle_element") {
+
+      // Send a message to all open tabs to toggle elements
+      chrome.tabs.query({}, function (tabs) {
+        for (const tab of tabs) {
+          chrome.tabs.sendMessage(tab.id, { hideElements: message.hideElements });
+        }
+      });
+    }
+  });
+  
--- content.js
+++ content.js
@@ -1,5 +1,38 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.message === "toggle_element") {
-      sendResponse({ message: "element_toggled" }); 
+const xpathsToToggle = [
+  "/html/body/div[1]/div/div/div[1]/div/div[1]/div[3]/div/div[2]/span/span", // xpath for notification badge
+  "//*[@id='__next']/div/div/div[1]/div/div[1]/div[3]/div/div[1]/span/span", // xpath for message badge
+  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[2]" // xpath for posts on feed
+];
+
+function toggleElements(xpathExpressions, shouldHide) {
+  xpathExpressions.forEach(xpathExpression => {
+    const xpathResult = document.evaluate(xpathExpression, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+    let element = xpathResult.iterateNext();
+
+    while (element) {
+      element.style.display = shouldHide ? "none" : ""; 
+      element = xpathResult.iterateNext();
     }
+  });
+}
+
+function toggleAndSyncElements() {
+  chrome.storage.sync.get("hideElements", function (data) {
+    const shouldHide = !data.hideElements;
+    chrome.storage.sync.set({ hideElements: shouldHide });
+    toggleElements(xpathsToToggle, shouldHide);
+
+    // Send a message to other tabs to update their state
+    chrome.runtime.sendMessage({ hideElements: shouldHide });
+  });
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.message === "toggle_element") {
+    toggleAndSyncElements();
+  }
+});
+
+chrome.storage.sync.get("hideElements", function (data) {
+  toggleElements(xpathsToToggle, data.hideElements);
 });
