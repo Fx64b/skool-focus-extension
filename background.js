d68eba990296f27774cdc10459525244ad6593e0
Fx64b <90456762+Fx64b@users.noreply.github.com>
2023-10-07 08:29:29 +0200

    chore(format): apply code formatting

--- background.js
+++ background.js
@@ -1,44 +1,44 @@
 function toggleElement() {
-    chrome.tabs.query({}, function (tabs) {
-      for (const tab of tabs) {
-        if (tab.favIconUrl && tab.url.includes("skool.com")) {
-          chrome.tabs.sendMessage(tab.id, { message: "toggle_element" });
-        }
+  chrome.tabs.query({}, function (tabs) {
+    for (const tab of tabs) {
+      if (tab.favIconUrl && tab.url.includes('skool.com')) {
+        chrome.tabs.sendMessage(tab.id, { message: 'toggle_element' });
       }
-    });
-  }
-  
+    }
+  });
+}
+
 // Toggle elements on all open tabs
 function toggleElementsOnAllTabs() {
+  chrome.tabs.query({}, function (tabs) {
+    for (const tab of tabs) {
+      if (tab.favIconUrl && tab.url.includes('skool.com')) {
+        chrome.tabs.sendMessage(tab.id, { message: 'toggle_element' });
+      }
+    }
+  });
+}
+
+// Add listener for browser action click
+chrome.action.onClicked.addListener(function (tab) {
+  toggleElementsOnAllTabs();
+});
+
+// Add a listener for the message from the content script to toggle elements.
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.message === 'toggle_element') {
+    // Send a message to all open tabs to toggle elements
     chrome.tabs.query({}, function (tabs) {
       for (const tab of tabs) {
-        if (tab.favIconUrl && tab.url.includes("skool.com")) {
-          chrome.tabs.sendMessage(tab.id, { message: "toggle_element" });
-        }
+        chrome.tabs.sendMessage(tab.id, { hideElements: message.hideElements });
       }
     });
   }
-  
-  // Add listener for browser action click
-  chrome.action.onClicked.addListener(function (tab) {
-    toggleElementsOnAllTabs();
-  });
-  
-  // Add a listener for the message from the content script to toggle elements.
-  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.message === "toggle_element") {
-      // Send a message to all open tabs to toggle elements
-      chrome.tabs.query({}, function (tabs) {
-        for (const tab of tabs) {
-          chrome.tabs.sendMessage(tab.id, { hideElements: message.hideElements });
-        }
-      });
-    }
-  });
-  
-  // Add a listener for tab update
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.favIconUrl && tab.url.includes("skool.com")) {
-        chrome.tabs.sendMessage(tab.id, { message: "tab_update" });
-    }
-  });
+});
+
+// Add a listener for tab update
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (tab.favIconUrl && tab.url.includes('skool.com')) {
+    chrome.tabs.sendMessage(tab.id, { message: 'tab_update' });
+  }
+});
--- content.js
+++ content.js
@@ -1,71 +1,75 @@
 const allXpaths = [
-  "//*[@id='__next']/div/div/div[1]/div/div[1]/div[3]/div/div[2]/span/span", // notification badge
-  "//*[@id='__next']/div/div/div[1]/div/div[1]/div[3]/div/div[1]/span/span", // message badge
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[2]", // posts on community feed (also hides video in classroom section, unwanted)
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]", // "write something", tags and "previous - next" (also hides video in classroom section, unwanted) 
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[3]", // "previous - next" navigation
-  "//*[@id='__next']/div/div/div[3]/div/div[3]/div", // community sidebar + 30 day leaderboard
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[1]", // community tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[3]", // calendar tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[4]", // members tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[5]", // leaderboard tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[6]", // about tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[1]/div[3]/div/div[2]/span/span', // notification badge
+  '//*[@id="__next"]/div/div/div[1]/div/div[1]/div[3]/div/div[1]/span/span', // message badge
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[2]', // posts on community feed (also hides video in classroom section, unwanted)
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]', // 'write something', tags and 'previous - next' (also hides video in classroom section, unwanted)
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[3]', // 'previous - next' navigation
+  '//*[@id="__next"]/div/div/div[3]/div/div[3]/div', // community sidebar + 30 day leaderboard
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[1]', // community tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[3]', // calendar tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[4]', // members tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[5]', // leaderboard tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[6]', // about tab link
 ];
 
 // -----------------------------
 // different blocking categories
 // -----------------------------
 
-const notifications= [
-  "//*[@id='__next']/div/div/div[1]/div/div[1]/div[3]/div/div[1]/span/span", // notification badge
-  "//*[@id='__next']/div/div/div[1]/div/div[1]/div[3]/div/div[2]/span/span", // message badge
+const notifications = [
+  '//*[@id="__next"]/div/div/div[1]/div/div[1]/div[3]/div/div[1]/span/span', // notification badge
+  '//*[@id="__next"]/div/div/div[1]/div/div[1]/div[3]/div/div[2]/span/span', // message badge
 ];
 
 const tabLinks = [
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[1]", // community tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[3]", // calendar tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[4]", // members tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[5]", // leaderboard tab link
-  "//*[@id='__next']/div/div/div[1]/div/div[2]/a[6]", // about tab link
-]
-
-const communityFeed = [
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[2]", // all posts on the community feed
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[3]", // "previous - next" navigation
-  "//*[@id='__next']/div/div/div[3]/div/div[3]/div", // community sidebar + 30 day leaderboard
-]
-
-const communityFeedHeader = [
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[1]/div/div[1]", // "write something"
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[1]/div/div[2]", // upcoming events
-  "//*[@id='__next']/div/div/div[3]/div/div[1]/div/div/div[1]/div[1]/div/div[3]", // categories
-]
-
-const classroomSection = [
-  "//*[@id='__next']/div/div/div[3]/div/div/div[2]/div/div", 
-  "//*[@id='__next']/div/div/div[3]/div/div/div[1]/div/div[1]", // Title and completion status
-  "//*[@id='__next']/div/div/div[3]/div/div/div[2]/div/div/div[2]", // post content
-  "//*[@id='__next']/div/div/div[3]/div/div/div[1]/div/div[1]/div[2]" // completion status
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[1]', // community tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[3]', // calendar tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[4]', // members tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[5]', // leaderboard tab link
+  '//*[@id="__next"]/div/div/div[1]/div/div[2]/a[6]', // about tab link
 ];
 
+const communityFeed = [
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[2]', // all posts on the community feed
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[3]', // 'previous - next' navigation
+  '//*[@id="__next"]/div/div/div[3]/div/div[3]/div', // community sidebar + 30 day leaderboard
+];
+
+const communityFeedHeader = [
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[1]/div/div[1]', // 'write something'
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[1]/div/div[2]', // upcoming events
+  '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div[1]/div/div[3]', // categories
+];
+
+const classroomSection = [
+  '//*[@id="__next"]/div/div/div[3]/div/div/div[2]/div/div',
+  '//*[@id="__next"]/div/div/div[3]/div/div/div[1]/div/div[1]', // Title and completion status
+  '//*[@id="__next"]/div/div/div[3]/div/div/div[2]/div/div/div[2]', // post content
+  '//*[@id="__next"]/div/div/div[3]/div/div/div[1]/div/div[1]/div[2]', // completion status
+];
 
 const elementsToHide = allXpaths;
 
 // fixes issue with hidden classroom content elements
 function fixClassroomSection() {
-  if(window.location.href.includes("/classroom")) {
+  if (window.location.href.includes('/classroom')) {
     toggleElements(classroomSection, false);
   }
 }
 
-
 function toggleElements(xpathExpressions, shouldHide) {
-  xpathExpressions.forEach(xpathExpression => {
-    const xpathResult = document.evaluate(xpathExpression, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+  xpathExpressions.forEach((xpathExpression) => {
+    const xpathResult = document.evaluate(
+      xpathExpression,
+      document,
+      null,
+      XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+      null,
+    );
     let element = xpathResult.iterateNext();
 
     while (element) {
-      element.style.display = shouldHide ? "none" : "initial"; 
+      element.style.display = shouldHide ? 'none' : 'initial';
       element = xpathResult.iterateNext();
     }
   });
@@ -73,10 +77,10 @@ function toggleElements(xpathExpressions, shouldHide) {
 
 // listen for messages from the background script
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.message === "toggle_element") {
+  if (message.message === 'toggle_element') {
     toggleAndSyncElements();
-  } else if (message.message === "tab_update") {
-    chrome.storage.sync.get("hideElements", function (data) {
+  } else if (message.message === 'tab_update') {
+    chrome.storage.sync.get('hideElements', function (data) {
       toggleElements(elementsToHide, data.hideElements);
       fixClassroomSection();
     });
@@ -85,7 +89,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
 // Function to toggle elements and sync the state using chrome.storage.sync.
 function toggleAndSyncElements() {
-  chrome.storage.sync.get("hideElements", function (data) {
+  chrome.storage.sync.get('hideElements', function (data) {
     const shouldHide = !data.hideElements;
     chrome.storage.sync.set({ hideElements: shouldHide });
     toggleElements(elementsToHide, shouldHide);
@@ -97,6 +101,6 @@ function toggleAndSyncElements() {
 }
 
 // Retrieve state from chrome.storage.sync and toggle
-chrome.storage.sync.get("hideElements", function (data) {
+chrome.storage.sync.get('hideElements', function (data) {
   toggleAndSyncElements(elementsToHide);
 });
