bcda633f7e09b629ea2d0492d27ac04f64001140
Fx64b <90456762+Fx64b@users.noreply.github.com>
2023-10-15 09:01:19 +0200

    feat(popup): update toggle button state according to stored values

--- popup.js
+++ popup.js
@@ -3,9 +3,25 @@ document.addEventListener('DOMContentLoaded', function () {
       toggleAll: 'all',
       toggleNotifications: 'notifications',
       toggleCommunityFeed: 'communityFeed'
-      
     };
-  
+
+    // Function to update the UI state of toggle buttons
+    function updateToggleButtonState() {
+      chrome.storage.sync.get('hideElements', function (data) {
+        const currentHiddenStatus = data.hideElements || {};
+
+        Object.keys(toggleButtons).forEach(buttonId => {
+          const section = toggleButtons[buttonId];
+          const button = document.getElementById(buttonId);
+
+          // Update the UI state based on stored value
+          button.checked = currentHiddenStatus[section];
+        });
+      });
+    }
+
+    updateToggleButtonState();
+
     Object.keys(toggleButtons).forEach(buttonId => {
       const button = document.getElementById(buttonId);
       button.addEventListener('click', function () {
@@ -14,11 +30,13 @@ document.addEventListener('DOMContentLoaded', function () {
           const currentHiddenStatus = data.hideElements || {};
           currentHiddenStatus[section] = !currentHiddenStatus[section];
           chrome.storage.sync.set({ hideElements: currentHiddenStatus });
-  
+
           // Send a message to content scripts to update their state
           chrome.tabs.query({}, function (tabs) {
             tabs.forEach(tab => {
-              chrome.tabs.sendMessage(tab.id, { message: 'tab_update' });
+              if (tab.favIconUrl && tab.url.includes('skool.com')) {
+                chrome.tabs.sendMessage(tab.id, { message: 'tab_update' });
+              }
             });
           });
         });
